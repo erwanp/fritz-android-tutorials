586e8133b458d7a6c37906677d4a614579cca9e3
hsiaoer <eric@fritz.ai>
2019-05-07 12:14:45 -0400

    create a demo pose estimation app

---
+++ PoseEstimationApp/app/src/main/java/ai/fritz/camera/MainActivity.java
@@ -0,0 +1,73 @@
+package ai.fritz.camera;
+
+import android.graphics.Canvas;
+import android.media.Image;
+import android.util.Size;
+
+import ai.fritz.core.Fritz;
+import ai.fritz.poseestimationmodel.PoseEstimationOnDeviceModel;
+import ai.fritz.vision.FritzVision;
+import ai.fritz.vision.FritzVisionImage;
+import ai.fritz.vision.FritzVisionOrientation;
+import ai.fritz.vision.poseestimation.FritzVisionPosePredictor;
+import ai.fritz.vision.poseestimation.FritzVisionPoseResult;
+
+
+public class MainActivity extends LiveCameraActivity {
+
+    private static final String API_KEY = "bbe75c73f8b24e63bc05bf81ed9d2829";
+
+    FritzVisionPosePredictor predictor;
+    FritzVisionImage visionImage;
+    FritzVisionPoseResult poseResult;
+
+    @Override
+    protected void initializeFritz() {
+        // TODO: Uncomment this and modify your api key above.
+         Fritz.configure(this, API_KEY);
+    }
+
+    @Override
+    protected void setupPredictor() {
+        // STEP 1: Get the predictor and set the options.
+        // ----------------------------------------------
+        // A FritzOnDeviceModel object is available when a model has been
+        // successfully downloaded and included with the app.
+        PoseEstimationOnDeviceModel poseEstimationOnDeviceModel = new PoseEstimationOnDeviceModel();
+        predictor = FritzVision.PoseEstimation.getPredictor(poseEstimationOnDeviceModel);
+
+        // ----------------------------------------------
+        // END STEP 1
+    }
+
+    @Override
+    protected void setupImageForPrediction(Image image) {
+        // Set the rotation
+        int imageRotation = FritzVisionOrientation.getImageRotationFromCamera(this, cameraId);
+        // STEP 2: Create the FritzVisionImage object from media.Image
+        // ------------------------------------------------------------------------
+        visionImage = FritzVisionImage.fromMediaImage(image, imageRotation);
+        // ------------------------------------------------------------------------
+        // END STEP 2
+    }
+
+    @Override
+    protected void runInference() {
+        // STEP 3: Run predict on the image
+        // ---------------------------------------------------
+        poseResult = predictor.predict(visionImage);
+        // ----------------------------------------------------
+        // END STEP 3
+    }
+
+    @Override
+    protected void showResult(Canvas canvas, Size cameraSize) {
+        // STEP 4: Draw the prediction result
+        // ----------------------------------
+        if(poseResult != null) {
+            poseResult.drawPoses(canvas, cameraSize);
+        }
+        // ----------------------------------
+        // END STEP 4
+    }
+}
